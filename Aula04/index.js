b4e0dc0e4590e8c1e5a273d7de54284a18797edb
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-07-27 23:05:35 -0300

    .

---
+++ Aula04/index.js
@@ -0,0 +1,56 @@
+/* OPERADORES ARITIMÉTICOS */
+
+// (+) SOMA
+let valor01 = 15 + 15;
+console.log(`15 + 15 = ${valor01}`);
+
+valor01++ // pos incremento de +1
+console.log(valor01);
+
+++valor01 // pre incremento de +1
+console.log(valor01);  
+
+valor01 += 8 ;// valor01 = valor01 + 8
+console.log(valor01);
+
+// (+) CONCATENAÇÃO
+const valor02 = '15' + '15';
+console.log(`'15' + '15' = ${valor02}`);
+
+// (-) SUBTRAÇÃO
+let valor03 = 45 - 15.5;
+console.log(`45 - 15.5 = ${valor03}`);
+
+valor03--;  // pos decremento de -1
+console.log(valor03);
+
+--valor03; // pre decremento de -1
+console.log(valor03); 
+
+valor03 -= 0.5; // valor03 = valor03 - 0.5
+console.log(valor03);
+
+// (/) DIVISÃO
+let valor04 = 10 / 2;
+console.log(`10/2 = ${valor04}`);
+
+valor04 /= 5;
+console.log(valor04);
+
+// (%) RESTO DA DIVISÃO
+const valor05 = 25 % 2;
+console.log(`Resto da Divisão de 25/2 = ${valor05}`)
+
+// (*) MULTIPLICAÇÃO
+let valor06 = 10 * 4;
+console.log(`10 * 4 = ${valor06}`);
+
+valor06 *= 2;
+console.log(valor06);
+
+// (**) POTÊNCIA
+let valor07 = 2 ** 3;
+console.log(`2^3 = ${valor07}`);
+
+valor07 **= 2;
+console.log(valor07);
